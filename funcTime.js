97170513435ae55df9077a9e54d12997918bfbc6
Nitai j. Perez <nitai.perez@ironsrc.com>
2015-03-18 18:48:12 +0200

    Main module initial commit. Created object with callback timing method

---
+++ funcTime.js
@@ -0,0 +1,50 @@
+/**
+ * Created by Nitai J. Perez
+ * nitai.perez@gmail.com
+ * 18/03/2015.
+ */
+
+var _times = {};
+
+function time(label) {
+    // Init label:
+    if (!this._times[label]) this._times[label] = {calls: 0};
+
+    this._times[label].timestamp = Date.now();
+
+}
+function timeEnd(label) {
+    if (!this._times[label]) {
+        throw new Error('No such label: ' + label);
+    }
+
+    // Label called, increment count:
+    this._times[label].calls += 1;
+
+    // Set duration:
+    var duartion = Date.now() - this._times[label];
+
+    // Update average:
+    this._times[label].avg =
+        this._times[label].avg
+            ? ((avg * this._times[label].calls - 1) + duartion) / this._times[label].calls
+            : duartion;
+
+    // log:
+    console.log('%s: %dms (avg: %dms across %s calls)', label, duartion, this._times[label].avg, this._times[label].calls);
+}
+
+/**
+ * Wrap a callback in order to time it's call time.
+ */
+function cbTime(label, cb) {
+    time(label);
+    return function () {
+        timeEnd(label);
+        cb.apply(this, arguments);
+    }
+}
+
+module.exports = {
+    cbTime: cbTime
+};
